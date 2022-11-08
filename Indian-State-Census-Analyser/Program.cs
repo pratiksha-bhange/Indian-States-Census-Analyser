d1d31fb242da643ccf216159fa37d307ec02c591
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-11-08 18:02:06 +0530

    [Pratu]ADDED: Welcome to Indian States Census Analyser Program

---
+++ Indian-State-Census-Analyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Indian_State_Census_Analyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Indian State Census Analyser Program");
+        }
+    }
+}
